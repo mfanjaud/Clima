10037a7f142bb1ebabfcec05ad981f369c098171
mfanjaud <contact@marionfanjaud.fr>
2018-03-19 12:08:41 +0100

    update 19.03.2

--- Clima/Clima/Controller/WeatherViewController.swift
+++ Clima/Clima/Controller/WeatherViewController.swift
@@ -88,7 +88,7 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, Change
     
     func updateUIWithWeatherData() {
         cityLabel.text = weatherDataModel.city
-        temperatureLabel.text = String(weatherDataModel.temperature)
+        temperatureLabel.text = "\(weatherDataModel.temperature)°"
         weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
         
     }
@@ -129,7 +129,8 @@ class WeatherViewController: UIViewController, CLLocationManagerDelegate, Change
     
     //Write the userEnteredANewCityName Delegate method here:
     func userEnteredNewCityName(city: String) {
-        print(city)
+        let params : [String : String] = ["q" : city, "appid" : APP_ID ]
+        getWeatherData(url: WEATHER_URL, parameters: params)
     }
     
     
